4efb3df77f50e21d85b320681c3dcede54155bcc
daichi0410 <59831107+daichi0410@users.noreply.github.com>
2020-01-16 15:44:18 +0900

    ログアウト機能〜flashオブジェクト機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
-  def edit    
+
+  def edit
   end
 
   def update
     if current_user.update(user_params)
       redirect_to root_path
-    else 
+    else
       render :edit
     end
-end
+  end
 
-private
+  private
 
   def user_params
     params.require(:user).permit(:name, :email)
